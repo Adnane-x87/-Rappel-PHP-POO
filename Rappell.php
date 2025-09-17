ef659cf5bbf7e2c52637adf9c963bb2a6b3b5a7d
Adnane-x87 <tangerinoh40@gmail.com>
2025-09-17 10:51:10 +0100

    new commit

--- Rappell.php
+++ Rappell.php
@@ -7,3 +7,26 @@ $articles = [
   ['id'=>3,'title'=>'Composer & Autoload','category'=>'outils','views'=>90,'author'=>'Amina','published'=>false, 'tags'=>['composer','php']],
   ['id'=>4,'title'=>'Validation FormRequest','category'=>'laravel','views'=>210,'author'=>'Sara','published'=>true,  'tags'=>['laravel','validation']],
 ];
+
+
+function slugify(string $title): string {
+    $slug = strtolower($title);
+    $slug = preg_replace('/[^a-z0-9]+/i', '-', $slug);
+    return trim($slug, '-');
+}
+
+$published = array_values(
+  array_filter($articles, fn(array $a) => $a['published'] ?? false)
+);
+
+
+$light = array_map(
+  fn(array $a) => [
+    'id'    => $a['id'],
+    'title' => $a['title'],
+    'slug'  => slugify($a['title']),
+    'views' => $a['views'],
+  ],
+  $published
+);
+
